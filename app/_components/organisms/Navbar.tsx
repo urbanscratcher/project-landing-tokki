cb3b1f53d52c460f9f9860f4c2d81347e0eeb2f8
Joun <urbanscratcher@gmail.com>
2023-05-31 05:38:55 +0900

    s size done

--- app/_components/organisms/Navbar.tsx
+++ app/_components/organisms/Navbar.tsx
@@ -1,36 +1,84 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { SyntheticEvent, useEffect, useState } from "react";
 
 import logo from "@/public/images/logo.png";
 import classNames from "classnames";
 import Image from "next/image";
+import { MdMenu as MenuIcon } from "react-icons/md";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => {
   const [belowNav, setBelowNav] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
-    const scrollHandler = () => {
-      const scrollPosition = window.scrollY;
+    if (window.innerWidth >= 700) {
+      const scrollHandler = () => {
+        const scrollPosition = window.scrollY;
 
-      if (scrollPosition <= 82.5) {
-        setBelowNav(false);
-      }
+        if (scrollPosition <= 82.5) {
+          setBelowNav(false);
+        }
 
-      if (scrollPosition > 82.5) {
-        setBelowNav(true);
-      }
-    };
+        if (scrollPosition > 82.5) {
+          setBelowNav(true);
+        }
+      };
 
-    window.addEventListener("scroll", scrollHandler);
+      window.addEventListener("scroll", scrollHandler);
+
+      return () => {
+        window.removeEventListener("scroll", scrollHandler);
+      };
+    }
   }, [belowNav]);
 
+  useEffect(() => {
+    if (toggle) {
+      const ulEl = document.querySelector("nav ul");
+      const svgEl = document.querySelector("nav div svg");
+      const divEl = document.querySelector("nav div");
+      const pathsEl = [...document.querySelectorAll("nav div path")];
+
+      const handleClick = (e: any) => {
+        if (
+          e.target !== ulEl &&
+          e.target !== svgEl &&
+          e.target !== divEl &&
+          !pathsEl.includes(e.target)
+        ) {
+          setToggle(!toggle);
+        }
+      };
+      document.addEventListener("click", (e) => handleClick(e));
+      return () => {
+        window.removeEventListener("click", (e) => handleClick(e));
+      };
+    }
+  }, [toggle]);
+
+  const openHandler = () => {
+    console.log("clicked");
+    if (!toggle) {
+      console.log("toggle assessed: false");
+      setToggle(true);
+    }
+  };
+
+  const realignHandler = () => {
+    return belowNav && !toggle
+      ? classNames(styles.nav, styles.realign)
+      : styles.nav;
+  };
+
   return (
-    <nav
-      className={belowNav ? classNames(styles.nav, styles.realign) : styles.nav}
-    >
-      <ul>
+    <nav className={realignHandler()}>
+      <div onClick={openHandler} className={styles.mobileIcon}>
+        <MenuIcon />
+      </div>
+
+      <ul className={toggle ? styles.toggleNav : undefined}>
         <li>
           <a href="#about">about</a>
         </li>
